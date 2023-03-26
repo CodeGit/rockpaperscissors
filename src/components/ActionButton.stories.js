f24a2213406825b8087c1981236dd2960e51e82f
CodeGit <matloob@gmail.com>
2023-03-26 00:04:11 +0000

    testing some responsive button sizes

--- src/components/ActionButton.stories.js
+++ src/components/ActionButton.stories.js
@@ -1,4 +1,5 @@
-import ActionButton from "./ActionButton";
+import { Button } from "@mui/material";
+import ActionButton, { LizardAction, PaperAction, ScissorsAction, SpockAction } from "./ActionButton";
 import { RockAction } from "./ActionButton";
 
 export default {
@@ -7,3 +8,9 @@ export default {
 };
 
 export const Rock = () => <ActionButton action={RockAction} />;
+export const Paper = () => <ActionButton action={PaperAction} />;
+export const Scissors = () => <ActionButton action={ScissorsAction} />;
+export const Lizard = () => <ActionButton action={LizardAction} />;
+export const Spock = () => <ActionButton action={SpockAction} />;
+
+export const Test = () => <Button >Test</Button>
--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -6,6 +6,25 @@ import PaperLogo from "../assets/paper.svg";
 import ScissorsLogo from "../assets/scissors.svg";
 import LizardLogo from "../assets/lizard.svg";
 import SpockLogo from "../assets/spock.svg";
+import { css } from "@mui/material";
+
+import PropTypes, { string } from "prop-types";
+
+const styles = css({
+    display: "flex",
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+});
+
+const labelStyles = css({
+    fontSize: "4vw",
+});
+
+const imgStyles = css({
+    height: "15vh",
+    width: "15vw",
+});
 
 export interface Action {
     name: string,
@@ -16,15 +35,21 @@ export const RockAction= {name: "Rock", icon: RockLogo};
 export const PaperAction = {name: "Paper", icon: PaperLogo};
 export const ScissorsAction = {name: "Scissors", icon: ScissorsLogo};
 export const LizardAction = {name: "Lizard", icon: LizardLogo};
-export const SpockAction = {name: "Spck", icon: SpockLogo};
+export const SpockAction = {name: "Spock", icon: SpockLogo};
 
 const rpsActions = [RockAction, PaperAction, ScissorsAction];
 const rpslsActions = [RockAction, PaperAction, ScissorsAction, LizardAction, SpockAction];
 
-const ActionButton: React.FC<Action> = (action) => {
-    return (<Button>
-        <img src={action.icon} alt={action.name}/>
-    </Button>);
+const ActionButton: React.FC<{action: Action, onClick: Function}> = ({action, onClick}) => {
+    console.log(`Action ${action.name}`);
+    return (
+        <div css={styles}>
+            <Button>
+                <img src={action.icon} css={imgStyles}/>
+            </Button>
+            <span css={labelStyles}>{action.name}</span>
+        </div>
+    );
 }
 
 export default ActionButton;
