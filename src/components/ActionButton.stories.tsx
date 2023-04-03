a7d7029c52062c822fc2fa50ded3accfe9e0ae27
CodeGit <matloob@gmail.com>
2023-04-03 23:01:12 +0100

    set css styles for toggle button

--- src/components/ActionButton.stories.tsx
+++ src/components/ActionButton.stories.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "@emotion/react/jsx-runtime";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import ActionButton, { ActionProps, ActionType }  from "./ActionButton";
+import ActionButton, { ActionType }  from "./ActionButton";
 
 export default {
     title: "Action Button",
@@ -8,42 +8,10 @@ export default {
 } as ComponentMeta<typeof ActionButton>;
 
 const Template:ComponentStory<typeof ActionButton> = (args) => <ActionButton {...args} />
-let rockSelected = false;
-export const Rock = Template.bind({});
-Rock.args = {
+let selected = false;
+export const ExampleButton = Template.bind({});
+ExampleButton.args = {
     action: ActionType.Rock,
-    selected: rockSelected,
-    toggleSelected : () => {rockSelected = rockSelected ? false : true}
-};
-
-let  paperSelected = false;
-export const Paper = Template.bind({});
-Paper.args = {
-    action: ActionType.Paper,
-    selected: paperSelected,
-    toggleSelected : () => {paperSelected = paperSelected ? false : true}
-};
-
-let  scissorsSelected = false;
-export const Scissors = Template.bind({});
-Scissors.args = {
-    action: ActionType.Scissors,
-    selected: scissorsSelected,
-    toggleSelected : () => {scissorsSelected = scissorsSelected ? false : true}
-};
-
-let  lizardSelected = false;
-export const Lizard = Template.bind({});
-Lizard.args = {
-    action: ActionType.Lizard,
-    selected: lizardSelected,
-    toggleSelected : () => {lizardSelected = lizardSelected ? false : true}
-};
-
-let spockSelected = false;
-export const Spock = Template.bind({});
-Spock.args = {
-    action: ActionType.Spock,
-    selected: spockSelected,
-    toggleSelected : () => {spockSelected = spockSelected ? false : true}
+    selected: selected,
+    toggleSelected : () => {selected = selected ? false : true}
 };
--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React, { ReactComponentElement } from "react";
-import { Button, IconButton } from "@mui/material";
+import { Button } from "@mui/material";
 import { ReactComponent as RockLogo } from "../assets/rock-outline.svg";
 import { ReactComponent as PaperLogo } from "../assets/paper-outline.svg";
 import { ReactComponent as ScissorsLogo } from "../assets/scissors-outline.svg";
@@ -32,17 +32,62 @@ const styles = css({
     textTransform: 'none',
     color: '7CA0DC',
     height: "1vh)",
+    backgroundColor: "rgba(0, 0, 0, 0)",
 });
 
-const imgStyles = css({
-    height: "clamp(1vh, calc(1em + 13vh), 30vh)",
-    width: "clamp(1vw, calc(1em + min-content), 30vw)",
+const roundButtonStyles = css({
+    height: "clamp(1em, calc(3.5em), 10em)",
+    width: "clamp(1em, calc(3.5em), 10em)",
+    top: "50%",
+    left: "50%",
+    borderRadius: "50%",
+    border: "5px solid #ececec",
+    cursor: "pointer",
+});
+
+const selectedRoundButtonStyles = css({
+    backgroundColor: "#0A3463",
+    boxShadow: 
+    "-1px -1px 10px rgba(255, 255, 255, 1), 1px 1px 10px rgba(70, 70, 70, 0.5)",
+    '&: hover': {
+        background: "rgba(135, 206, 235, 0.4)",
+        transition: "all 0.4s ease"
+    },
+    '&: active': {
+        background: "rgba(10, 52, 99, 0.4)",
+        boxShadow: 
+        "-10px -10px 10px rgba(255, 255, 255, 1), 10px 10px 10px rgba(70, 70, 70, 0.5)",
+        transition: "all 0.4s ease"
+    },
+}, roundButtonStyles);
+
+const unselectedRoundButtonStyles = css({
+    boxShadow: 
+    "-10px -10px 10px rgba(255, 255, 255, 1), 10px 10px 10px rgba(70, 70, 70, 0.5)",
+    backgroundColor: "#ffffff",
+    '&: hover': {
+        background: "rgba(135, 206, 235, 0.4)",
+        transition: "all 0.4s ease"
+    },
+    '&: active': {
+        background: "rgba(10, 52, 99, 1)",
+        boxShadow: 
+        "-1px -1px 10px rgba(255, 255, 255, 1), 1px 1px 10px rgba(70, 70, 70, 0.5)",
+        transition: "all 0.4s ease"
+    },
+}, roundButtonStyles);
+
+const svgStyles = css({
+    height: "clamp(1em, calc(2.5em), 10em)",
+    width: "clamp(1em, calc(2.5em), 10em)",
+    padding: "0.5em",
 });
 
 export const rpsGameActions = [ActionType.Rock, ActionType.Paper, ActionType.Scissors];
 export const rpslsGameActions = rpsGameActions.concat(ActionType.Lizard, ActionType.Spock) ;
 
 const ActionButton: React.FC<ActionProps> = ({action, selected, toggleSelected}) => {
+    // select appropriate SVG icon for type
     let Icon; 
     switch(action) {
         case ActionType.Rock:
@@ -61,11 +106,25 @@ const ActionButton: React.FC<ActionProps> = ({action, selected, toggleSelected})
             Icon = SpockLogo;
             break;
     }
+    // set styles and colours based on selected
+    let buttonStyles;
+    let strokeColour;
+    if (selected) {
+        buttonStyles = selectedRoundButtonStyles;
+        strokeColour = 'white';
+    } else {
+        buttonStyles = unselectedRoundButtonStyles;
+        strokeColour = 'black';
+    }
     return (
-            <Button css={styles} onClick={toggleSelected}>
-                <Icon />
-                <span>{action}</span>
-            </Button>
+            <div css={styles}>
+                <div 
+                    css={buttonStyles} 
+                    onClick={toggleSelected} 
+                    aria-label={action}>
+                    <Icon css={svgStyles} stroke={strokeColour} onClick={toggleSelected}/>
+                </div>
+            </div>
     );
 }
 
